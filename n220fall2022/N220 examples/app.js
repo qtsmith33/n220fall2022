a9f5fa23919954044befa477ea1471d3f27cf3c7
qtsmith33 <61795099+qtsmith33@users.noreply.github.com>
2022-08-31 17:10:08 -0400

    Inclass Examples1

---
+++ n220fall2022/N220 examples/app.js
@@ -0,0 +1,22 @@
+let myName = "Quinton";
+
+myName += ", the student";
+
+console.log(myName);
+
+let num = 10;
+let b = 20;
+
+console.log("before")
+// true
+if(num < b) {
+    console.log("during")
+}
+
+console.log("after")
+
+num = num + 5;
+num = num - 5;
+num = num * b;
+num = num b / 5;
+num = b % 2;
