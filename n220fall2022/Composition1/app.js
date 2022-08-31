df97e0f06f592b504fd2193f7c89b828ad6797b9
qtsmith33 <61795099+qtsmith33@users.noreply.github.com>
2022-08-31 18:13:42 -0400

    Composition1

---
+++ n220fall2022/Composition1/app.js
@@ -0,0 +1,35 @@
+// Quinton Smith
+// 8/31/2022
+// N220-24603 Fall 2022
+
+
+let myName = "Quinton";
+
+myName += ", the student";
+
+console.log(myName);
+
+let bestPofessor = "Fortunately led by";
+
+bestProfessor += ": Travis Faas";
+
+console.log(bestProfessor);
+
+let javaAdventure = "N220";
+
+console.log(javaAdventure);
+
+//Drawing a square at location (25, 25) with the sides of length 20
+fill('red');
+square(25, 25, 20);
+describe('red square with black outline at position (25, 25) on canvas - the walls of house');
+
+//Drawing a triangle for a roof
+fill('black');
+triangle(25, 0, 5, 5, 25, 5);
+describe('black rectangle with black outline with top point at 25,0 and touching the corners of the box made above - a flat roof of house');
+
+//Drawing a rectangle for a door
+fill('brown')
+rect(15, 15, 10);
+describe('brown rectangle with black outline at bottom half of square');
