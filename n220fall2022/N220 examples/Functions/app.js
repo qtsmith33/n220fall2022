cd1ab6b4ce55695ff091d629196d0aeda249fd83
qtsmith33 <61795099+qtsmith33@users.noreply.github.com>
2022-09-08 16:35:29 -0400

    Activities 9/1/22-9/8/22
    Activities 9/1/22-9/8/22

---
+++ n220fall2022/N220 examples/Functions/app.js
@@ -0,0 +1,24 @@
+function setup() {
+    createCanvas(800,600);
+}
+
+function drawFace(x,y) {
+    
+    line(x, y, x+10, y+10)
+    circle(x + 10, y + 10, 5);
+    circle(x + 30, y + 10, 5);
+    circle(x + 15, y + 30, 5);
+}
+
+function drawFace(x,y) {
+background(0)
+//invoke the function
+drawFace(400, 400);
+//top left
+drawFace(10, 10);
+//top right
+drawFace(400, 10);
+//where the mouse is
+drawFace(mouseX, mouseY);
+}
+
