5395fe5bbffd8d0f6233d3e9bd4aab3cef7ad71e
qtsmith33 <61795099+qtsmith33@users.noreply.github.com>
2022-09-15 22:26:34 -0400

    Functions Exercise1-1 update

---
+++ n220fall2022/Functions Exercises 1/Exercise1-3/app.js
@@ -0,0 +1,16 @@
+//Quinton Smith
+//9/15/2022
+//N220 Fall 2022
+
+//setup canvas, establish a as variable.
+let a;
+function setup() {
+    createCanvas(400, 400)
+    a=0
+}
+
+//draw function, increments a and writes in console
+function draw() {
+    a++;
+    console.log(a)
+}2
